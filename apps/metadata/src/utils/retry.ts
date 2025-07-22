63f93c1949c1474e04223b61b89a49a8c20a28d3
bloodnighttw <bbeenn1227@gmail.com>
2025-07-22 15:01:56 +0800

    Add fetchAllServer function and retry utility for server version fetching

---
+++ apps/metadata/src/utils/retry.ts
@@ -0,0 +1,18 @@
+export default async function retry<U>(promise: ()=>Promise<U>, retries = 3, delay = 1000): Promise<U> {
+    let attempt = 0;
+
+    while (attempt < retries) {
+        try {
+            return await promise();
+        } catch (_) {
+            attempt++;
+            console.error(`Attempt ${attempt} failed!`);
+            if (attempt >= retries) {
+                throw new Error(`Failed after ${retries} attempts`);
+            }
+            await new Promise(resolve => setTimeout(resolve, delay));
+        }
+    }
+
+    throw new Error("This should never be reached");   
+}
