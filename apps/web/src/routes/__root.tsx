5e96e20c4003459cb43283eae31596112320f4c6
bloodnighttw <bbeenn1227@gmail.com>
2025-07-23 14:25:49 +0800

    chore: add dev tool for tanstack router

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,10 +1,12 @@
 import { createRootRoute, Outlet, type ErrorComponentProps } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
 export const Route = createRootRoute({
   	component: () => {
-		return (
+		return <>
 			<Outlet />
-		)
+			<TanStackRouterDevtools/>
+		</>
 	},
 	errorComponent: ({ error }: ErrorComponentProps) => {
 		return <div>error : {error.stack}</div>
